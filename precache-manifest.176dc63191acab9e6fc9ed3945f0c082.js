6ea705fc6f0b29ce0cf2de2b69de21d85a74152f
Thomas Chin <thomasc246@gmail.com>
2020-03-24 13:34:58 -0700

    Updates

---
+++ precache-manifest.176dc63191acab9e6fc9ed3945f0c082.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c631826163c07d4ce7ab2b157e7c319d",
+    "url": "/thomaschin/index.html"
+  },
+  {
+    "revision": "e94d7ebe9518f4fd364f",
+    "url": "/thomaschin/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "30e92da9228f6b6b287f",
+    "url": "/thomaschin/static/css/main.57ae85a7.chunk.css"
+  },
+  {
+    "revision": "e94d7ebe9518f4fd364f",
+    "url": "/thomaschin/static/js/2.85784559.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/thomaschin/static/js/2.85784559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30e92da9228f6b6b287f",
+    "url": "/thomaschin/static/js/main.3615ac4f.chunk.js"
+  },
+  {
+    "revision": "ac9bf786e5738959eee7",
+    "url": "/thomaschin/static/js/runtime-main.b6ba697b.js"
+  },
+  {
+    "revision": "9e36c8fd15d6f93ed6a45f9484746e8f",
+    "url": "/thomaschin/static/media/010-linkedin.9e36c8fd.svg"
+  },
+  {
+    "revision": "5b23a837268b09d0efa9e307023c1dae",
+    "url": "/thomaschin/static/media/011-instagram.5b23a837.svg"
+  },
+  {
+    "revision": "7f281199258d96e249a7fce4101006b9",
+    "url": "/thomaschin/static/media/Helvetica Neu Bold.7f281199.ttf"
+  },
+  {
+    "revision": "ddc250127189a3b4ee4802e6c2493757",
+    "url": "/thomaschin/static/media/github.ddc25012.svg"
+  },
+  {
+    "revision": "b917c7aff159881c67950f1ab5f4c366",
+    "url": "/thomaschin/static/media/test-pic-lol.b917c7af.jpg"
+  },
+  {
+    "revision": "07549b12ed665bc4001b76ff254644b7",
+    "url": "/thomaschin/static/media/thomas-chin-engineer.07549b12.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.20a2cf4a272747b8426f714ce240e69a.js"
+  "/thomaschin/precache-manifest.176dc63191acab9e6fc9ed3945f0c082.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thomaschin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
