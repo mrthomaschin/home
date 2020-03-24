f9d39da7eb188acda520038ebe2d1e0985fddc0b
Thomas Chin <thomasc246@gmail.com>
2020-03-24 13:04:29 -0700

    Changing main url

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { Router } from "react-router-dom";
-import createHistory from "history/createBrowserHistory";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <Router history={createHistory({ basename: process.env.PUBLIC_URL })}>
+  // <Router history={createHistory({ basename: process.env.PUBLIC_URL })}>
+  //   <App />
+  // </Router>,
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
   </Router>,
   document.getElementById("root")
